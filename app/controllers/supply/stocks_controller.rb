67aafb2656703b15648d243f168338de20aa1f87
beslow <549174542@qq.com>
2015-11-20 23:07:34 +0800

    fix bug

--- app/controllers/supply/stocks_controller.rb
+++ app/controllers/supply/stocks_controller.rb
@@ -1,5 +1,9 @@
 class Supply::StocksController < BaseController
   def index
     @stocks = current_user.company.stocks
+    unless params[:product_name].blank?
+      @stocks = @stocks.joins(:general_product)
+      @stocks = @stocks.where("general_products.name like ?", "%#{params[:product_name]}%")
+    end
   end
 end
