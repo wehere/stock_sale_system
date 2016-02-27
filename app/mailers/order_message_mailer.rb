779a894d18d8470d11f5bcde4227f25ef4d7cd7e
beslow <549174542@qq.com>
2016-02-27 17:26:37 +0800

    抄送

--- app/mailers/order_message_mailer.rb
+++ app/mailers/order_message_mailer.rb
@@ -6,7 +6,7 @@ class OrderMessageMailer < ApplicationMailer
     @customer = Company.find(customer_id)
     @need_reach_date = need_reach_date.to_date.to_s(:db)
     address = Company.find(supplier_id).users.collect {|u| u.email}
-    mail(to: address.shift, recient: address, subject: "#{@customer.simple_name}的订单")
+    mail(to: address.shift, cc: address, subject: "#{@customer.simple_name}的订单")
   end
 
   def question_order_message content, send_date, order_id
@@ -16,7 +16,7 @@ class OrderMessageMailer < ApplicationMailer
     @store = @order.store
     @send_date = send_date.to_s(:db)
     address = @order.supplier.users.collect {|u| u.email}
-    mail(to: address.shift, recient: address, subject: "#{@customer.simple_name}对我们的单据产生了疑问")
+    mail(to: address.shift, cc: address, subject: "#{@customer.simple_name}对我们的单据产生了疑问")
   end
 
   def reply_question_order_message content, send_date, order_id
@@ -26,13 +26,13 @@ class OrderMessageMailer < ApplicationMailer
     @store = @order.store
     @send_date = send_date.to_s(:db)
     address = @order.customer.users.collect {|u| u.email}
-    mail(to: address.shift, recient: address, subject: "来自#{@supplier.simple_name}对单据疑问的回复")
+    mail(to: address.shift, cc: address, subject: "来自#{@supplier.simple_name}对单据疑问的回复")
   end
 
   def send_to_supplier send_order_message_id
     @send_order_message = SendOrderMessage.find_by_id(send_order_message_id)
     address = @send_order_message.supplier.mail_address.split(',')
-    mail(to: address.shift, recient: address, subject: "#{@send_order_message.customer.simple_name}-#{@send_order_message.store.name} 的订单")
+    mail(to: address.shift, cc: address, subject: "#{@send_order_message.customer.simple_name}-#{@send_order_message.store.name} 的订单")
   end
 
 end
