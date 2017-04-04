a2f604d2632f7066527e6d7ee92aa3fe05b24854
beslow <549174542@qq.com>
2017-04-05 00:55:03 +0800

    默认显示本月

--- app/controllers/supply/month_inventories_controller.rb
+++ app/controllers/supply/month_inventories_controller.rb
@@ -1,5 +1,6 @@
 class Supply::MonthInventoriesController < BaseController
   def index
+    params[:year_month_id] ||= YearMonth.current_year_month.id
     @month_inventories = MonthInventory.where(year_month_id: params[:year_month_id]||YearMonth.current_year_month.id)
     .where(supplier_id: current_user.company.id)
     .order(updated_at: :desc)
