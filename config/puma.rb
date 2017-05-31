1d100bbd04f7ea8f3926ff8e9614a1c9fe1032f9
beslow <549174542@qq.com>
2017-05-31 23:13:43 +0800

    changeram130

--- config/puma.rb
+++ config/puma.rb
@@ -14,7 +14,7 @@ before_fork do
   require 'puma_worker_killer'
 
   PumaWorkerKiller.config do |config|
-    config.ram           = 300 # mb
+    config.ram           = 130 # mb
     config.frequency     = 5    # seconds
     config.percent_usage = 0.98
     config.rolling_restart_frequency = 12 * 3600 # 12 hours in seconds
