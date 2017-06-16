d1da27714006d792fc0f5ddf1745c8a502e706db
beslow <549174542@qq.com>
2017-06-16 22:56:45 +0800

    去掉一些存在性校验

--- app/models/ding/score.rb
+++ app/models/ding/score.rb
@@ -1,7 +1,8 @@
 class Ding::Score < ApplicationRecord
   self.table_name = 'ding_scores'
 
-  validates :uploaded_at, :rank, :health, :performance, :average_load_time, :business, :quality, :security, :response_time, :rpm, presence: true
+  validates :uploaded_at, presence: true
+  #, :rank, :health, :performance, :average_load_time, :business, :quality, :security, :response_time, :rpm
 
   validates :uploaded_at, uniqueness: true
 end
